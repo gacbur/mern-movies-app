e56227767265bcbddd8f07ddd1f564ba25218c3f
Kacper <kacper0775@gmail.com>
2021-01-27 15:33:32 +0100

    add footer functionality

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import {
+    AiFillTwitterCircle,
+    AiFillInstagram,
+    AiFillFacebook
+} from 'react-icons/ai'
+
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className="footer">
-            Footer
+            <div className="footer__text">
+                <h3>Our socialmedia</h3>
+            </div>
+            <div className="footer__icons">
+                <Link to="/"><AiFillTwitterCircle className="icon" /></Link>
+                <Link to="/"><AiFillInstagram className="icon" /></Link>
+                <Link to="/"><AiFillFacebook className="icon" /></Link>
+            </div>
         </div>
     )
 }
